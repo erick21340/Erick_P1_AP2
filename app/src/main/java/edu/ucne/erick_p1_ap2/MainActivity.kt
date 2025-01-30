221e4956016687cc9bd868a50b707b752a8c5874
erick21340 <Torot0086@gmail.com>
2025-01-29 19:46:22 -0900

    carga de los archivos de proyecto

---
+++ app/src/main/java/edu/ucne/erick_p1_ap2/MainActivity.kt
@@ -0,0 +1,28 @@
+package edu.ucne.erick_p1_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.erick_p1_ap2.ui.theme.Erick_P1_AP2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Erick_P1_AP2Theme {
+
+                }
+            }
+        }
+    }
+
+
