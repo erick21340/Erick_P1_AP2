dfb2f1b08cb0c3b505adfb890e07a792605c1600
erick21340 <Torot0086@gmail.com>
2025-02-05 19:58:04 -0900

    eh creado el AppModule

--- app/src/main/java/edu/ucne/erick_p1_ap2/di/AppModule.kt
+++ app/src/main/java/edu/ucne/erick_p1_ap2/di/AppModule.kt
@@ -8,14 +8,24 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import androidx.room.Room
+import edu.ucne.erick_p1_ap2.data.local.database.SistemaDB
+
 
 @InstallIn(SingletonComponent::class)
 @Module
-object  AppModule {
+object AppModule {
 
     @Provides
     @Singleton
-    fun provideTecnicoDB(@ApplicationContext appContext: Context) = appContext
+    fun provideSistemaDB(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            SistemaDB::class.java,
+            "SistemaDB.db"
+        ).fallbackToDestructiveMigration()
+            .build()
 
-
+    @Provides
+    fun provideSistemaDao(sistemaDB: SistemaDB) = sistemaDB.sistemaDao()
 }
